046d4aef83ff532e939ba26a9d84c6b19b69e64e
chan04luis <xorashk.l.angel@gmail.com>
2025-01-08 21:56:45 -0600

    (hotfix) mamadas de luis jorge

--- Models/Request/Seguridad/ModuloRequest.cs
+++ Models/Request/Seguridad/ModuloRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,15 @@ namespace Models.Request.Seguridad
 {
     public class ModuloRequest
     {
+        [JsonProperty("IdModulo")]
         public Guid? uIdModulo { get; set; }
+        [JsonProperty("ClaveModulo")]
         public string sClaveModulo { get; set; }
+        [JsonProperty("NombreModulo")]
         public string sNombreModulo { get; set; }
+        [JsonProperty("PathModulo")]
         public string sPathModulo { get; set; }
+        [JsonProperty("MostrarEnMenu")]
         public bool bMostrarEnMenu { get; set; }
     }
 }
