567bea70367663bb1ed67f4bb6f4855d4f14c85b
chan04luis <144571639+phantomaster01@users.noreply.github.com>
2025-01-08 22:44:14 -0600

    (hotfix) mamadas de luis jorge

--- Models/Request/Seguridad/PaginaRequest.cs
+++ Models/Request/Seguridad/PaginaRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,17 @@ namespace Models.Request.Seguridad
 {
     public class PaginaRequest
     {
+        [JsonProperty("IdPagina")]
         public Guid? uIdPagina { get; set; }
+        [JsonProperty("IdModulo")]
         public Guid uIdModulo { get; set; }
+        [JsonProperty("ClavePagina")]
         public string sClavePagina { get; set; }
+        [JsonProperty("NombrePagina")]
         public string sNombrePagina { get; set; }
+        [JsonProperty("PathPagina")]
         public string sPathPagina { get; set; }
+        [JsonProperty("MostrarEnMenu")]
         public bool bMostrarEnMenu { get; set; }
     }
 }
--- Models/Request/Seguridad/PerfilRequest.cs
+++ Models/Request/Seguridad/PerfilRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace Models.Request.Seguridad
 {
     public class PerfilRequest
     {
+        [JsonProperty("idPerfil")]
         public Guid? IdPerfil { get; set; }
 
         [JsonPropertyName("clavePerfil")]
