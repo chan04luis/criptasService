2eae11f3b4ae94b6f22711b22469a2137067ecb7
phantomaster01 <phantom_master01@hotmail.com>
2025-02-26 12:03:44 -0600

    (feat) updates

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17304cdb77fa4abd85ffeac976aff42c48bad042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94762d78df0c29fe9e0ff009d12acca51404a0d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
