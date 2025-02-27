11aff458b71064a9ce935a8e42cb4c1efdf0b12b
chan04luis <xorashk.l.angel@gmail.com>
2025-02-26 20:49:09 -0600

    movil

--- Api/Controllers/MovilController.cs
+++ Api/Controllers/MovilController.cs
@@ -1,11 +1,11 @@
 ﻿using Business.Implementation.Catalogos;
 using Business.Interfaces.Catalogos;
 using Business.Interfaces.Seguridad;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Models.Models;
 using Models.Request.Clientes;
+using Models.Responses.Criptas;
+using Models.Responses.Servicio;
 using Models.Seguridad;
 using Swashbuckle.AspNetCore.Annotations;
 using Utils;
@@ -20,12 +20,18 @@ namespace Api.Controllers
         private readonly IBusClientes _busClientes;
         private readonly ILogger<MovilController> _logger;
         private readonly IBusServicios _busServicios;
-        public MovilController(IBusAutenticacion _busAutenticacion, IBusClientes busClientes, ILogger<MovilController> logger, IBusServicios busServicios)
+        private readonly IBusIglesias _busIglesias;
+        private readonly IBusCriptas _busCriptas;
+
+        public MovilController(IBusAutenticacion _busAutenticacion, IBusClientes busClientes, ILogger<MovilController> logger, 
+            IBusServicios busServicios, IBusIglesias busIglesias, IBusCriptas busCriptas)
         {
             this._busAutenticacion = _busAutenticacion;
             _busClientes = busClientes;
             _logger = logger;
             _busServicios = busServicios;
+            _busIglesias = busIglesias;
+            _busCriptas = busCriptas;
         }
 
         [HttpGet("servicio/{id}")]
@@ -52,6 +58,7 @@ namespace Api.Controllers
             return StatusCode((int)response.HttpCode, response);
 
         }
+
         [HttpPost("nuevo")]
         [SwaggerOperation(Summary = "Crea un cliente", Description = "Valida los datos y guarda un nuevo cliente en la base de datos.")]
         public async Task<Response<bool>> CreateClient([FromBody] EntClienteRequest cliente)
@@ -71,6 +78,56 @@ namespace Api.Controllers
                 _logger.LogInformation("Cliente creado exitosamente con ID: {Id}", response.Result?.uId);
             }
             return respuesta;
+        }
+
+        [HttpGet("List")]
+        [SwaggerOperation(Summary = "Obtiene la lista de iglesias", Description = "Recupera una lista de todas las iglesias.")]
+        public async Task<Response<List<EntIglesias>>> GetIglesiaList()
+        {
+            _logger.LogInformation("Iniciando recuperación de lista de iglesias.");
+            var response = await _busIglesias.GetIglesiaList();
+            if (response.HasError)
+            {
+                _logger.LogWarning("Error al recuperar lista de iglesias: {Error}", response.Message);
+            }
+            else
+            {
+                _logger.LogInformation("Lista de iglesias recuperada exitosamente. Total: {Count}", response.Result?.Count);
+            }
+            return response;
+        }
+
+        [HttpGet("ListActive/{uIdIglesia}")]
+        public async Task<Response<List<EntServiceItem>>> GetServicioListActive(Guid uIdIglesia)
+        {
+            _logger.LogInformation("Iniciando recuperación de lista de servicios activos.");
+            var response = await _busServicios.GetListActive(uIdIglesia);
+            if (response.HasError)
+            {
+                _logger.LogWarning("Error al recuperar lista de servicios activos: {Error}", response.Message);
+            }
+            else
+            {
+                _logger.LogInformation("Lista de servicios activos recuperada exitosamente. Total: {Count}", response.Result?.Count);
+            }
+            return response;
+        }
+
+        [HttpGet("ListDisponible/Iglesia/{uId}")]
+        [SwaggerOperation(Summary = "Obtiene la lista de criptas", Description = "Recupera una lista de todas las criptas.")]
+        public async Task<Response<List<CriptasDisponibles>>> GetCriptaListDisponibleByIglesia(Guid uId)
+        {
+            _logger.LogInformation("Iniciando recuperación de lista de criptas.");
+            var response = await _busCriptas.BGetListDisponibleByIglesia(uId);
+            if (response.HasError)
+            {
+                _logger.LogWarning("Error al recuperar lista de criptas: {Error}", response.Message);
+            }
+            else
+            {
+                _logger.LogInformation("Lista de criptas recuperada exitosamente. Total: {Count}", response.Result?.Count);
+            }
+            return response;
         }
     }
 }
